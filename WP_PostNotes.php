3c77d325f1dcd4c90b01f7a92fb2f6b1e55b7433
damianm666 <damianm666@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-14 08:58:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-postnotes/trunk@45775 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WP_PostNotes.php
+++ WP_PostNotes.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.dcoda.co.uk/index.php/downloads/wordpress/wp_postnotes/
 Description: WP_PostnNotes allows you to add private note to a post that will only be viewable by the author or named users.
 Author: DCoda Ltd
 Author URI: http://www.dcoda.co.uk
-Version: 2.5
+Version: 2.5.0
 */
 require_once(dirname(__FILE__).'/library/classes/base.php');
 class DCodaPostNotes extends dc_base_2_1_0  {
