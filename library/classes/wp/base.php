e7ad37e0a918b9e5ad5d7aa3aad5147725f48bd3
damianm666 <damianm666@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-16 14:53:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-postnotes/trunk@46191 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- library/classes/wp/base.php
+++
@@ -1,5 +0,0 @@
-<?php
-class dc_wp_base_1_0_0 extends dc_base_2_2_0 {
-}
-
-?>
--- library/classes/wp/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-class dc_wp_footer_1_0_0 extends dc_base_2_2_0 {
-	function init() {
-		add_action('wp_footer', $this->callback);
-	}
-}
-?>
